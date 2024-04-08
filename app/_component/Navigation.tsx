b7b4778c87b8fc9db0c39cfde429dd64e1bed20d
byoungyoon <bur5698@naver.com>
2024-04-08 18:28:35 +0900

    init navbor 제작

---
+++ app/_component/Navigation.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import styles from './navigation.module.css';
+import { useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
+import { CSSTransition } from 'react-transition-group';
+
+export default function Navigation() {
+  const [top, setTop] = useState(true);
+  const nodeRef = useRef(null);
+
+  const onScroll = () => {
+    setTop(window.scrollY === 0);
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', onScroll);
+
+    return () => document.removeEventListener('scroll', onScroll);
+  }, []);
+
+  return (
+    <CSSTransition
+      nodeRef={nodeRef}
+      classNames={{
+        enter: styles['navbar-enter-active'],
+        exit: styles['navbar-exit-active'],
+        exitDone: styles['navbar-exit-done'],
+      }}
+      timeout={500}
+      in={top}
+    >
+      <nav ref={nodeRef} className={styles.navbar}>
+        <div className={styles.container}>
+          <Link className={styles.title} href={'/#page-top'}>
+            BY Portfolio
+          </Link>
+          <ul className={styles.nav}>
+            <li>
+              <Link href={'/#skills'}>Skills</Link>
+            </li>
+            <li>
+              <Link href={'/#archiving'}>Archiving</Link>
+            </li>
+            <li>
+              <Link href={'/#projects'}>Projects</Link>
+            </li>
+            <li>
+              <Link href={'/#career'}>Career</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </CSSTransition>
+  );
+}
