a773acff564ac561662844d2386136e501839b88
byoungyoon <bur5698@naver.com>
2024-04-09 20:16:59 +0900

    :fire: 배경이미지 변경

---
+++ app/_component/CustomSakura.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { Sakura } from '@byoungyoon/by-asset';
+
+type Props = {
+  children: ReactNode;
+  total: number;
+};
+
+export default function CustomSakura({ children, total }: Props) {
+  return (
+    <div>
+      <Sakura total={total}>{children}</Sakura>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,26 @@
 import styles from './page.module.css';
 import Navigation from '@/app/_component/Navigation';
+import CustomSakura from '@/app/_component/CustomSakura';
 
 export default function Home() {
   return (
     <main id="page-top" className={styles.main}>
-      <Navigation />
-      <header className={styles.header}>header</header>
-      <section id="skills" className={styles.skillSection}>
-        skills
-      </section>
-      <section id="archiving" className={styles.archivingSection}>
-        archiving
-      </section>
-      <section id="projects" className={styles.projectSection}>
-        project
-      </section>
-      <section id="career" className={styles.careerSection}>
-        career
-      </section>
+      <CustomSakura total={80}>
+        <Navigation />
+        <header className={styles.header}>header</header>
+        <section id="skills" className={styles.skillSection}>
+          skills
+        </section>
+        <section id="archiving" className={styles.archivingSection}>
+          archiving
+        </section>
+        <section id="projects" className={styles.projectSection}>
+          project
+        </section>
+        <section id="career" className={styles.careerSection}>
+          career
+        </section>
+      </CustomSakura>
       <footer className={styles.footer}>footer</footer>
     </main>
   );
