7c00d2b34a1293407196f479ee3e33f0ba0531a8
byoungyoon <bur5698@naver.com>
2024-04-23 05:09:00 +0900

    :bug: 테스트 버그 해결
    1. window 객체 접근 버그 해결

--- app/_component/CustomTree.tsx
+++ app/_component/CustomTree.tsx
@@ -5,36 +5,35 @@ import styles from './customTree.module.css';
 import { useColor } from 'react-color-palette';
 import 'react-color-palette/css';
 import { Tree } from '@byoungyoon/by-asset';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CustomTreeOption from '@/app/_component/CustomTreeOption';
 
 const sakuraColor = '#ffc5d0';
 
 export default function CustomTree() {
+  const [option, setOption] = useState({
+    reset: 0,
+    level: 3,
+  });
+
   const [open, setOpen] = useState(false);
-  const [reset, setReset] = useState(0);
   const [color, setColor] = useColor(sakuraColor);
-  const level = window.innerHeight / 250 - 1;
 
   const onToggle = () => {
     setOpen(!open);
   };
 
   const onReset = () => {
-    setReset(reset + 1);
+    setOption({ ...option, reset: option.reset + 1 });
   };
+
+  useEffect(() => {
+    setOption({ ...option, level: window.innerHeight / 250 - 1 });
+  }, []);
 
   return (
     <div className={styles.container}>
-      <Tree
-        target="header"
-        color={color.hex}
-        reset={reset}
-        isResize={true}
-        defaultDepth={12}
-        defaultCount={2}
-        level={level}
-      />
+      <Tree target="header" isResize={true} color={color.hex} defaultDepth={12} defaultCount={3} {...option} />
       <div className={styles.optionLayer}>
         {open && <CustomTreeOption color={color} setColor={setColor} onReset={onReset} />}
         <div className={styles.optionButton} onClick={onToggle}>
