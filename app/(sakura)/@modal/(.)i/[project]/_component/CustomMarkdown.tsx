c5d2a33a8559eb0bc14d96bac4556ac117d84443
byoungyoon <bur5698@naver.com>
2024-04-27 21:38:45 +0900

    :fire: readme 추가

---
+++ app/(sakura)/@modal/(.)i/[project]/_component/CustomMarkdown.tsx
@@ -0,0 +1,100 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { coldarkCold } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import remarkGfm from 'remark-gfm';
+import ReactMarkdown from 'react-markdown';
+import styles from './customMarkdown.module.css';
+
+type Props = {
+  markdown: string;
+};
+
+export default function CustomMarkdown({ markdown }: Props) {
+  return (
+    <ReactMarkdown
+      components={{
+        h2: (props: any) => {
+          const { children, className, node, ...rest } = props;
+
+          return (
+            <h2 className={styles.h2} {...rest}>
+              {children}
+            </h2>
+          );
+        },
+        h3: (props: any) => {
+          const { children, className, node, ...rest } = props;
+
+          return (
+            <h3 className={styles.h3} {...rest}>
+              {children}
+            </h3>
+          );
+        },
+        h4: (props: any) => {
+          const { children, className, node, ...rest } = props;
+
+          return (
+            <h3 className={styles.h4} {...rest}>
+              {children}
+            </h3>
+          );
+        },
+
+        p: (props: any) => {
+          const { children, className, node, ...rest } = props;
+
+          return (
+            <p className={styles.p} {...rest}>
+              {children}
+            </p>
+          );
+        },
+
+        a: (props: any) => {
+          const { children, className, node, ...rest } = props;
+
+          return (
+            <a className={styles.a} target="_blank" {...rest}>
+              {children}
+            </a>
+          );
+        },
+
+        ul: (props: any) => {
+          const { children, className, node, ...rest } = props;
+
+          return <ul className={styles.ul}>{children}</ul>;
+        },
+
+        table: (props: any) => {
+          const { children, className, node, ...rest } = props;
+
+          return <table className={styles.table}>{children}</table>;
+        },
+
+        img: (props: any) => {
+          const { className, node, ...rest } = props;
+
+          return <img className={styles.img} {...rest} />;
+        },
+
+        code: (props: any) => {
+          const { children, className, node, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || '');
+          return match ? (
+            <SyntaxHighlighter {...rest} PreTag="div" language={match[1]} style={coldarkCold}>
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+      rehypePlugins={[remarkGfm]}
+    >
+      {markdown}
+    </ReactMarkdown>
+  );
+}
--- app/(sakura)/@modal/(.)i/[project]/readme/page.tsx
+++ app/(sakura)/@modal/(.)i/[project]/readme/page.tsx
@@ -1,49 +1,16 @@
-import ReactMarkdown from 'react-markdown';
-import { readFileSync } from 'fs';
-import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { coldarkCold } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { data } from '@/app/_data/projectsData';
+import CustomMarkdown from '@/app/(sakura)/@modal/(.)i/[project]/_component/CustomMarkdown';
+import { data } from '@/app/_data/readmeData';
 
 type Props = {
   project: string;
 };
 
 export default function Page({ params }: { params: Props }) {
-  const markdownContent = readFileSync(`app/_data/readme/${params.project}.md`, 'utf-8');
+  const markdownContent = data[params.project];
 
   return (
     <div>
-      <ReactMarkdown
-        components={{
-          h2: (props: any) => {
-            const { children, className, node, ...rest } = props;
-
-            return (
-              <h2 style={{ marginBottom: '10px' }} {...rest}>
-                {children}
-              </h2>
-            );
-          },
-
-          code: (props: any) => {
-            const { children, className, node, ...rest } = props;
-            const match = /language-(\w+)/.exec(className || '');
-            return match ? (
-              <SyntaxHighlighter {...rest} PreTag="div" language={match[1]} style={coldarkCold}>
-                {String(children).replace(/\n$/, '')}
-              </SyntaxHighlighter>
-            ) : (
-              <code {...rest} className={className}>
-                {children}
-              </code>
-            );
-          },
-        }}
-        rehypePlugins={[remarkGfm]}
-      >
-        {markdownContent}
-      </ReactMarkdown>
+      <CustomMarkdown markdown={markdownContent} />
     </div>
   );
 }
